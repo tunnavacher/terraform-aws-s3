22719b9337541b4fcabe611a5a77473d929fdadc
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-06-18 13:53:44 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -6,7 +6,7 @@ output "bucket_arn" {
   value = aws_s3_bucket.bucket_source_data.arn
 }
 
-output "bucket_domain_name " {
+output "bucket_domain_name" {
   value = aws_s3_bucket.bucket_source_data.bucket_domain_name 
 }
 

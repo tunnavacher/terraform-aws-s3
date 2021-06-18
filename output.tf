84917fcf9d43da1a55e4619b1a474f330dce99d4
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-06-18 13:52:08 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,19 @@
+output "bucket_id" {
+  value = aws_s3_bucket.bucket_source_data.id
+}
+
+output "bucket_arn" {
+  value = aws_s3_bucket.bucket_source_data.arn
+}
+
+output "bucket_domain_name " {
+  value = aws_s3_bucket.bucket_source_data.bucket_domain_name 
+}
+
+output "bucket_region" {
+  value = aws_s3_bucket.bucket_source_data.region 
+}
+
+output "bucket_tags_all" {
+  value = aws_s3_bucket.bucket_source_data.tags_all 
+}

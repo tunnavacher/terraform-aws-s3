12b48c0f347f2c8010fc96876aa9c3049095b6de
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-07-06 12:51:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,46 +20,46 @@ resource "aws_s3_bucket" "bucket_source_data" {
       }
     }
   }
-  
+
 # Define actions for S3 to take during an object's lifetime such as transitioning objects to another storage class and archiving
-  lifecycle_rule {
-    id      = var.lifecycle_rule_id
-    enabled = var.lifecycle_rule_enabled
+ # lifecycle_rule {
+  #  id      = var.lifecycle_rule_id
+  #  enabled = var.lifecycle_rule_enabled
 
-    prefix = var.prefix
+  #  prefix = var.prefix
 
-    transition {
-      days          = var.standard_ia_days
-      storage_class = var.standard_ia
-    }
+  #  transition {
+    #  days          = var.standard_ia_days
+    #  storage_class = var.standard_ia
+    #}
 
-    transition {
-      days          = var.glacier_days
-      storage_class = var.glacier
-    }
+  #  transition {
+   #   days          = var.glacier_days
+   #   storage_class = var.glacier
+  #  }
 
-    expiration {
-      days = var.expiration_days
-    }
-  }
-  
+   # expiration {
+   #   days = var.expiration_days
+   # }
+  #}
+ 
   # To define options you want Amazon S3 to apply during replication 
-  replication_configuration {
-    role = var.sameaccount_replication_rule
+  #replication_configuration {
+   # role = var.sameaccount_replication_rule
 
-    rules {
-      prefix = var.prefix
-      status = var.replication_rules_enabled
+    #rules {
+    #  prefix = var.prefix
+      #status = var.replication_rules_enabled
 
-      destination {
-        bucket             = var.destination_bucket
-        replica_kms_key_id = var.replica_kms_id
-      }
+     # destination {
+      #  bucket             = var.destination_bucket
+    #    replica_kms_key_id = var.replica_kms_id
+     # }
 
-      source_selection_criteria {
-        sse_kms_encrypted_objects {
-          enabled = var.replicate_objects_encryption
-        }
+     # source_selection_criteria {
+     #   sse_kms_encrypted_objects {
+       #   enabled = var.replicate_objects_encryption
+      #  }
       }
     }
   }
